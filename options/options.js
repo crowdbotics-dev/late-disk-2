b4374ae021be7860f19064d0ce9f522a56ce656b
Crowdbotics <engineering@crowdbotics.com>
2024-04-03 19:32:26 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-disk-2",
+  url: "https://late-disk-2.botics.co",
+  api: "https://late-disk-2.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
